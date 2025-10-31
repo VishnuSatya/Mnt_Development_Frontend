f752af4f5db5706656a5b37294efc23088e731f7
Vishnu <vishnusatya9596@hotmail.com>
2025-10-31 05:36:27 -0400

    chenges

--- src/app/guards/auth-guard.spec.ts
+++ src/app/guards/auth-guard.spec.ts
@@ -1,17 +1,22 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import { Router } from '@angular/router';
 
-import { authGuard } from './auth-guard';
+import { AuthGuard } from './auth-guard';
+import { AuthService } from '../services/auth-service';
 
-describe('authGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => authGuard(...guardParameters));
-
+describe('AuthGuard', () => {
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        AuthGuard,
+        { provide: AuthService, useValue: { isLoggedIn: () => true } },
+        { provide: Router, useValue: { createUrlTree: () => '/login' } },
+      ],
+    });
   });
 
   it('should be created', () => {
-    expect(executeGuard).toBeTruthy();
+    const guard = TestBed.inject(AuthGuard);
+    expect(guard).toBeTruthy();
   });
 });
